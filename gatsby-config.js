f8f8aec5ce13008d729e9ac168b54e10c050ebe7
marthmf <canttouchtype@gmail.com>
2022-09-06 09:40:01 -0700

    Initial customizations

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+    siteTitle: `Septem Civita`,
+    siteTitleAlt: `Campaign - Septem Civita`,
+    siteHeadline: `A reference site for the Septem Civita Campaign and World`,
     siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteDescription: ``,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `mar`,
   },
   plugins: [
     {
@@ -30,15 +30,15 @@ module.exports = {
             title: `About`,
             slug: `/about`,
           },
+          {
+            title: `Maps & Images`,
+            slug: `/gallery`,
+          },
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `Discord`,
+            url: `https://discord.gg/EcFSfgck`,
           },
         ],
       },
